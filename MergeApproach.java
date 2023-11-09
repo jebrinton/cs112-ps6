29a3705923f8c56842c8375c25acfe18305ce1d3
jebrinton <122479901+jebrinton@users.noreply.github.com>
2023-11-09 00:05:09 -0500

    No idea how PairFinder doesn't pass the autograder

--- MergeApproach.java
+++ MergeApproach.java
@@ -1,5 +1,11 @@
-import java.awt.desktop.SystemSleepEvent;
-import java.sql.SQLSyntaxErrorException;
+/*
+ * CS 112 Problem Set 6
+ * Jacob Brinton
+ * jbrin@bu.edu
+ *
+ * This is a class of merging methods such as union and intersect.
+ */
+
 import java.util.Arrays;
 
 public class MergeApproach {
@@ -98,40 +104,99 @@ public class MergeApproach {
 
         return total;
     }
+
+    public static int[] intersect(int[] a1, int[] a2) {
+        if (a1 == null || a2 == null) {
+            throw new IllegalArgumentException();
+        }
+
+        int[] total = new int[Math.min(a1.length, a2.length)];
+        if (total.length == 0) {
+            return total;
+        }
+
+        if (a1.length > 1) {
+            Sort.quickSort(a1);
+        }
+        if (a2.length > 1) {
+            Sort.quickSort(a2);
+        }
+
+        int i = 0;
+        int j = 0;
+        int insert = 0;
+
+        while (i < a1.length && j < a2.length) {
+            // case for insertion is needed
+            if (a1[i] == a2[j] && a1[i] != total[insert]) {
+                total[insert++] = a1[i++];
+                j++;
+            }
+            // case for [j] > [i], so increment i++
+            else if (a1[i] < a2[j]) {
+                i++;
+            }
+            // case for [i] > [j], so increment j++
+            else if (a1[i] > a2[j]) {
+                j++;
+            }
+            // case for [insert], [i], and [j] all have the same value
+            else {
+                i++;
+                j++;
+            }
+        }
+        return total;
+    }
     public static void main(String[] args) {
-        int[] a1 = {10, 5, 7, 5, 9, 4};
-        int[] a2 = {7, 5, 15, 7, 7, 9, 10};
-        int[] result1 = union(a1, a2);
-        System.out.println("result1: " + Arrays.toString(result1));
+//        int[] a1 = {10, 5, 7, 5, 9, 4};
+//        int[] a2 = {7, 5, 15, 7, 7, 9, 10};
+//        int[] result1 = union(a1, a2);
+//        System.out.println("result1: " + Arrays.toString(result1));
+//
+//        int[] a3 = {0, 2, -4, 6, 10, 8};
+//        int[] a4 = {12, 0, -4, 8};
+//        int[] result2 = union(a3, a4);
+//        System.out.println("result2: " + Arrays.toString(result2));
+//
+//        int[] a5 = {9, 9, 9, 9, 9, 9, 9};
+//        int[] a6 = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 21};
+//        int[] result3 = union(a5, a6);
+//        System.out.println("result3: " + Arrays.toString(result3));
+//
+//        int[] a7 = {};
+//        int[] a8 = {14, 14, 14, 14, 21};
+//        int[] result4 = union(a7, a8);
+//        System.out.println("result4: " + Arrays.toString(result4));
+//
+//        int[] a9 = {0, 2};
+//        int[] a10 = {0, 0, 3, 1};
+//        int[] result5 = union(a9, a10);
+//        System.out.println("result5: " + Arrays.toString(result5));
+//
+//        int[] a11 = {0, 79, 13, 34, 50, 28, 52, 18, 17};
+//        int[] a12 = {90, 34, 12, 73, 92, 31};
+//        int[] result6 = union(a11, a12);
+//        System.out.println("result5: " + Arrays.toString(result6));
+//
+//        int[] a13 = {-99, 77, 17};
+//        int[] a14 = {0};
+//        int[] result7 = union(a13, a14);
+//        System.out.println("result5: " + Arrays.toString(result7));
 
-        int[] a3 = {0, 2, -4, 6, 10, 8};
-        int[] a4 = {12, 0, -4, 8};
-        int[] result2 = union(a3, a4);
-        System.out.println("result2: " + Arrays.toString(result2));
+        int[] b1 = {10, 5, 7, 5, 9, 4};
+        int[] b2 = {7, 5, 15, 7, 7, 9, 10};
+        int[] result90 = intersect(b1, b2);
+        System.out.println("Result b1: " + Arrays.toString(result90));
 
-        int[] a5 = {9, 9, 9, 9, 9, 9, 9};
-        int[] a6 = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 21};
-        int[] result3 = union(a5, a6);
-        System.out.println("result3: " + Arrays.toString(result3));
+        int[] b3 = {0, 2, -4, 6, 10, 8};
+        int[] b4 = {12, 0, -4, 8};
+        int[] result91 = intersect(b3, b4);
+        System.out.println("Result b2: " + Arrays.toString(result91));
 
-        int[] a7 = {};
-        int[] a8 = {14, 14, 14, 14, 21};
-        int[] result4 = union(a7, a8);
-        System.out.println("result4: " + Arrays.toString(result4));
-
-        int[] a9 = {0, 2};
-        int[] a10 = {0, 0, 3, 1};
-        int[] result5 = union(a9, a10);
-        System.out.println("result5: " + Arrays.toString(result5));
-
-        int[] a11 = {0, 79, 13, 34, 50, 28, 52, 18, 17};
-        int[] a12 = {90, 34, 12, 73, 92, 31};
-        int[] result6 = union(a11, a12);
-        System.out.println("result5: " + Arrays.toString(result6));
-
-        int[] a13 = {-99, 77, 17};
-        int[] a14 = {0};
-        int[] result7 = union(a13, a14);
-        System.out.println("result5: " + Arrays.toString(result7));
+        int[] b5 = {0, 0, 0, 2};
+        int[] b6 = {2, 0, 0, 0};
+        int[] result93 = intersect(b5, b6);
+        System.out.println("Result b3: " + Arrays.toString(result93));
     }
 }
--- PairFinder.java
+++ PairFinder.java
@@ -1,5 +1,17 @@
+/*
+ * CS 112 Problem Set 6
+ * Jacob Brinton
+ * jbrin@bu.edu
+ *
+ * This is a class that finds all pairs in an array that sum to a certain
+ * number.
+ */
+
 public class PairFinder {
     public static void findPairSums(int k, int[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException();
+        }
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
                 if (arr[i] + arr[j] == k) {
@@ -10,68 +22,105 @@ public class PairFinder {
     }
 
     public static void findPairSumsFaster(int k, int[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException();
+        }
         if (arr.length > 1) {
             Sort.quickSort(arr);
+        }
+        else if (arr.length == 0) {
+            return;
         }
 
         int i = 0;
         int j = 0;
-        while (j < arr.length && i < arr.length) {
-            // case of too large
-            if (arr[i] + arr[j] > k) {
-                break;
-            }
-            else if (arr[i] + arr[j] == k) {
+        do {
+            if (arr[i] + arr[j] == k) {
                 System.out.println(arr[i] + " + " + arr[j] + " = " + k);
-                // check for out of bounds
-                if (j+1 == arr.length) {
+                i++;
+            }
+            else if (arr[i] + arr[j] > k) {
+                // check j - 1 is in bounds
+                if (j - 1 < 0) {
+                    return;
+                }
+                // if going back to j-1 won't do anything
+                if (arr[i] + arr[j-1] < k) {
                     i++;
                 }
-                // only increment i if the difference between its next element
-                // is less than that of j's (this ensures j >= i always)
-                else if (arr[i+1] - arr[i] < arr[j+1] - arr[j]) {
+                else {
+                    j--;
+                }
+            } else {
+                if (j + 1 == arr.length || arr[i] + arr[j+1] > k) {
                     i++;
                 }
                 else {
                     j++;
                 }
             }
-            else {
-                // check for out of bounds
-                if (j+1 == arr.length) {
-                    i++;
-                }
-                // only increment i if the difference between its next element
-                // is less than that of j's (this ensures j >= i always)
-                else if (arr[i+1] - arr[i] < arr[j+1] - arr[j]) {
-                    i++;
-                }
-                else {
-                    j++;
-                }
-            }
-        }
+        } while (i <= j && i < arr.length && j < arr.length);
+
+//        while (j < arr.length && i < arr.length) {
+//            // case of too large
+//            if (arr[i] + arr[j] > k) {
+//                break;
+//            }
+//            else if (arr[i] + arr[j] == k) {
+//                System.out.println(arr[i] + " + " + arr[j] + " = " + k);
+//                // check for out of bounds
+//                if (j+1 == arr.length) {
+//                    i++;
+//                }
+//                // only increment i if the difference between its next element
+//                // is less than that of j's (this ensures j >= i always)
+//                else if (arr[i+1] - arr[i] < arr[j+1] - arr[j]) {
+//                    i++;
+//                }
+//                else {
+//                    j++;
+//                }
+//            }
+//            else {
+//                // check for out of bounds
+//                if (j+1 == arr.length) {
+//                    i++;
+//                }
+//                // only increment i if the difference between its next element
+//                // is less than that of j's (this ensures j >= i always)
+//                else if (arr[i+1] - arr[i] < arr[j+1] - arr[j]) {
+//                    i++;
+//                }
+//                else {
+//                    j++;
+//                }
+//            }
+//          }
     }
 
     public static void main(String[] args) {
-        findPairSums(12, new int[]{10, 4, 7, 7, 8, 5, 15});
-        findPairSums(12, new int[]{0, 0, 0, 0, 1, 2, 3, 4, 90});
-        findPairSums(-3, new int[]{90, -3, 0, -1, 2, -2, 56});
+//        findPairSums(12, new int[]{10, 4, 7, 7, 8, 5, 15});
+//        findPairSums(12, new int[]{0, 0, 0, 0, 1, 2, 3, 4, 90});
+//        findPairSums(-3, new int[]{90, -3, 0, -1, 2, -2, 56});
+//
+//        findPairSumsFaster(-5, new int[]{90, -3, 0, -1, 2, -2, 56});
+//        findPairSumsFaster(12, new int[]{10, 4, 7, 7, 8, 5, 15});
+//        findPairSumsFaster(12, new int[]{0, 0, 0, 0, 1, 2, 3, 4, 90});
+//        findPairSumsFaster(-3, new int[]{-2, -1, -2});
+//        findPairSumsFaster(0, new int[]{90, -2, 13, 0, -1, -2});
+//        findPairSumsFaster(1990, new int[]{995, 995, -2, 13, 0, -1, -2});
+//
+        findPairSumsFaster(10, new int[]{2, 3});
 
-        findPairSumsFaster(-5, new int[]{90, -3, 0, -1, 2, -2, 56});
-        findPairSumsFaster(12, new int[]{10, 4, 7, 7, 8, 5, 15});
-        findPairSumsFaster(12, new int[]{0, 0, 0, 0, 1, 2, 3, 4, 90});
-        findPairSumsFaster(-3, new int[]{90, 0, -2, 13, 0, -1, -2});
-        findPairSumsFaster(0, new int[]{90, -2, 13, 0, -1, -2});
-        findPairSumsFaster(1990, new int[]{995, 995, -2, 13, 0, -1, -2});
+//        findPairSumsFaster(100, new int[]{});
 
-        for (int i = -130; i < 140; i++) {
-            int[] randArr = new int[(int) (Math.random() * 15)];
-
-            for (int k = 0; k < randArr.length; k++) {
-                randArr[k] = (int)(Math.random() * (90 + 1)) - 45;
-            }
-            findPairSumsFaster(i, randArr);
-        }
+//        for (int i = -130; i < 140; i++) {
+//            int[] randArr = new int[(int) (Math.random() * 15)];
+//
+//            for (int k = 0; k < randArr.length; k++) {
+//                randArr[k] = (int)(Math.random() * (90 + 1)) - 45;
+//            }
+//            findPairSumsFaster(i, randArr);
+//        }
     }
 }
